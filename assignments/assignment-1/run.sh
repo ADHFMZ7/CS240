6fa608505359dcb10fd325c8a14b7f6b637b6e44
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-09-05 21:41:10 -0700

    switched to cpp isfloat

--- assignments/assignment-1/run.sh
+++ assignments/assignment-1/run.sh
@@ -1,7 +1,8 @@
 # compiles/assembles source files to object files
 nasm -f elf64 -o two_floats.o two_floats.asm
-nasm -f elf64 -o isfloat.o isfloat.asm
-g++ -c driver.cpp 
+#nasm -f elf64 -o isfloat.o isfloat.asm
+g++ -c -m64 -Wall -fno-pie -no-pie -o isfloat.o isfloat.cpp -std=c++17
+g++ -c driver.cpp -std=c++17
 
 #links files into a binary
 g++ -g -Wall -no-pie driver.o isfloat.o two_floats.o  -o output 

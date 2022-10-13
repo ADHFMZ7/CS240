bae59857e91e4a72d2866bd7f1e18f8dd7f13e07
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-10-13 10:03:53 -0700

    commit

--- assignments/midterm/run.sh
+++ assignments/midterm/run.sh
@@ -9,12 +9,14 @@ nasm -f elf64 -l manager.lis -o manager.o manager.asm
 
 nasm -f elf64 -l fill.lis -o fill.o fill.asm
 
+nasm -f elf64 -l reverse.lis -o reverse.o reverse.asm
+
 g++ -c -Wall -no-pie -m64 -std=c++17 -o display.o display.cpp
 
 g++ -c -Wall -m64 -no-pie -o driver.o driver.cpp -std=c++17
 
-g++ -m64 -no-pie -o output manager.o fill.o driver.o display.o -std=c++17
+g++ -m64 -no-pie -o output manager.o fill.o driver.o reverse.o display.o -std=c++17
 
 ./output
 
-rm *.o
+rm *.o *.lis

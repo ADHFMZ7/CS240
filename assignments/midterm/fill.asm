7c906bee7ee7130b964ff4ec4363322f4ce23ace
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-10-13 09:14:22 -0700

    commit

--- assignments/midterm/fill.asm
+++ assignments/midterm/fill.asm
@@ -1,45 +1,10 @@
-;****************************************************************************************************************************
-;Program name: "Add Float Array".
-; This program will allow a user to input float numbers in an array of size 6, and display the contents. It will also add
-; them together and display the result.
-; Copyright (C) 2021 Johnson Tong.                                                                           *
-;                                                                                                                           *
-;This file is part of the software program "Add Float Array".
-;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License   *
-;version 3 as published by the Free Software Foundation.                                                                    *
-;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied          *
-;warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
-;A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.                            *
-;****************************************************************************************************************************
-
-
-
-
-;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
 ;
-;Author information
-;  Author name: Johnson Tong
-;  Author email: jt28@csu.fullerton.edu
+; Ahmad Aldasouqi
+; ahmadaldasouqi@csu.fullerton.edu
+; October 13, 2022
+; Section TuTh 8-10am
 ;
-;Program information
-;  Program name: Add Float Array
-;  Programming languages: Assembly, C++, C, bash
-;  Date program began: 2021 March 10
-;  Date of last update: 2021 March 21
-;  Date of reorganization of comments: 2021 March 21
-;  Files in this program: control.asm, main.c, display.cc, sum.asm, fill.asm, run.sh
-;  Status: Finished.  The program was tested extensively with no errors in Tuffix 2020 Edition.
-;
-;This file
-;   File name: fill.asm
-;   Language: X86 with Intel syntax.
-;   Max page width: 132 columns
-;   Assemble: nasm -f elf64 -l fill.lis -o fill.o fill.asm
-;   Link: g++ -m64 -no-pie -o addFloatArray.out control.o fill.o main.o sum.o display.o -std=c++17
-;   Purpose: Defines the function that will take in the array from the control module and prompt the user to
-;            input floats into the array. Additionally, it will return the number of elements (not size) back to the
-;            caller.
-;========================================================================================================
+
 extern printf
 extern scanf
 
@@ -47,9 +12,7 @@ global fill
 
 segment .data
 
-enter_prompt db "Please enter up to 6 floating point numbers separated by ws,", 10, 0
-enter_prompt_two db "When finished press enter followed by Cntrl+D.", 10, 0
-float_format db "%ld", 0
+long_format db "%ld", 0
 
 segment .bss  ;Reserved for uninitialized data
 
@@ -82,19 +45,6 @@ push qword 0 ;staying on the boundary
 mov r15, rdi  ; This holds the first parameter (the array)
 mov r14, rsi  ; This holds the second parameter (the size of array)
 
-;Prompts:
-;"Please enter floating point numbers separated by ws,"
-;"When finished press enter followed by Cntrl+D."
-push qword 0
-mov rax, 0
-mov rdi, enter_prompt
-call printf
-pop rax
-push qword 0
-mov rax, 0
-mov rdi, enter_prompt_two
-call printf
-pop rax
 
 ; let user enter numbers until cntrl + d is entered
 ; this for loop will go to 6, the chosen array size, or end once cntrl d is pressed.
@@ -103,7 +53,7 @@ beginLoop:
   cmp r14, r13 ; we want to exit loop when we hit the size of array
   je outOfLoop
   mov rax, 0
-  mov rdi, float_format
+  mov rdi, long_format
   push qword 0
   mov rsi, rsp
   call scanf
@@ -137,5 +87,3 @@ pop rdi                                                     ;Restore rdi
 pop rbp                                                     ;Restore rbp
 
 ret
-
-;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**

11b7eb19d706e6d637a169ff3b12d70b172d5c19
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 04:10:25 -0800

    changed compile

--- assignments/assignment-5/run.sh
+++ assignments/assignment-5/run.sh
@@ -4,8 +4,8 @@ nasm -f elf64 -g manager.asm -o manager.o
 nasm -f elf64 -g randfill.asm -o fill.o
 
 g++ -m64 -Wall -fno-pie -no-pie quicksort.c -c -o quicksort.o -g
-g++ -m64 -Wall -fPIE -no-pie display.c -c -o display.o -g
+g++ -m64 -Wall -fno-pie -no-pie display.c -c -o display.o -g
 
 
 
-g++ driver.cpp manager.o quicksort.o display.o fill.o -o output -g
+g++ -m64 -Wall -fno-pie -no-pie driver.cpp manager.o quicksort.o display.o fill.o -o output -g

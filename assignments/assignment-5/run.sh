fac82f52eee18f3b08b711028d3aaa0782070c27
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 04:02:49 -0800

    fixed run

--- assignments/assignment-5/run.sh
+++ assignments/assignment-5/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+nasm -f elf64 -g manager.asm -o manager.o
 nasm -f elf64 -g randfill.asm -o fill.o
 gcc quicksort.c -c -o quicksort.o -g
-gcc test.c quicksort.o fill.o -o output -g
+gcc display.c -c -o display.o -g
+g++ driver.cpp manager.o quicksort.o display.o fill.o -o output -g

67799e96858e79410c51fcb9f1689abace38bfd7
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 15:07:32 -0800

    idk

--- assignments/final/run.sh
+++ assignments/final/run.sh
@@ -4,6 +4,6 @@ nasm -f elf64 -l sum.lis -o sum.o sum.asm
 nasm -f elf64 -l display.lis -o display.o display.asm
 nasm -f elf64 -l supervisor.lis -o supervisor.o supervisor.asm
 
-g++ -c -Wall -m64 -no-pie -o driver.o driver.cpp -std=c++17
-g++ -m64 -no-pie -o output supervisor.o randfill.o driver.o sum.o display.o -std=c++17
+#g++ -c -Wall -m64 -no-pie -o driver.o driver.cpp -std=c++17
+g++ -m64 -no-pie -o output driver.cpp supervisor.o randfill.o sum.o display.o -std=c++17
 

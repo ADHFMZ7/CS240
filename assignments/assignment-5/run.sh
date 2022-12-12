41c413f6f6630db5b5a6f81c45b13aab73de1342
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 04:05:24 -0800

    icommit

--- assignments/assignment-5/run.sh
+++ assignments/assignment-5/run.sh
@@ -2,6 +2,10 @@
 
 nasm -f elf64 -g manager.asm -o manager.o
 nasm -f elf64 -g randfill.asm -o fill.o
-gcc quicksort.c -c -o quicksort.o -g
-gcc display.c -c -o display.o -g
+
+g++ -m64 -Wall -fno-pie -no-pie quicksort.c -c -o quicksort.o -g
+g++ -m64 -Wall -fno-pie -no-pie display.c -c -o display.o -g
+
+
+
 g++ driver.cpp manager.o quicksort.o display.o fill.o -o output -g

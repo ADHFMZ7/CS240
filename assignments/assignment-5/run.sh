95684e8a9b7a4973230c1e60a2b1a533a0ebadb9
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 04:07:26 -0800

    run

--- assignments/assignment-5/run.sh
+++ assignments/assignment-5/run.sh
@@ -4,7 +4,7 @@ nasm -f elf64 -g manager.asm -o manager.o
 nasm -f elf64 -g randfill.asm -o fill.o
 
 g++ -m64 -Wall -fno-pie -no-pie quicksort.c -c -o quicksort.o -g
-g++ -m64 -Wall -fno-pie -no-pie display.c -c -o display.o -g
+g++ -m64 -Wall -fPIE -no-pie display.c -c -o display.o -g
 
 
 

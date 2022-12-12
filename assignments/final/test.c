f156924b56192dee8e18b70f05b25e0e9e6e3ade
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 14:46:10 -0800

    commit

--- assignments/final/test.c
+++ assignments/final/test.c
@@ -4,8 +4,8 @@ extern double sum(double* array, int count);
 
 int main()
 {
-    double a[] = {10.0, 20.0, 30.0};
-    int b = 3;
+    double a[] = {10.0, 20.0, 30.0, 40};
+    int b = 4;
 
     double c = sum(a, b);
     printf("%f\n", c);

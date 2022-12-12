ce8c0f9d05cb5baf8c5a75ebdc90589b2f0c2e60
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 15:41:14 -0800

    idk

--- assignments/final/display.asm
+++ assignments/final/display.asm
@@ -3,10 +3,28 @@ segment .data
 
 double_form: db "%1.12ld" ,10,0
 
+section .bss
 
+section .text
 
 global dislay
 
 display:
 
-ret
+    xor rcx, rcx
+    mov r8,  rdi ; array
+    mov r9, rsi ; count
+loop:
+    cmp rcx, r9 
+    jge done
+
+    movsd xmm0, [r8 + 8*r9]
+    mov rdi, double_form
+    call printf
+
+    inc rcx
+    jmp loop
+
+done:
+    
+    ret

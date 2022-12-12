ca5abe945c971ba0ae56c1d72e6813758577ed84
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 04:18:04 -0800

    changed sort algorithm

--- assignments/assignment-5/quicksort.c
+++ assignments/assignment-5/quicksort.c
@@ -1,36 +1,16 @@
-void q_sort(double numbers[], int left, int right)
-{
-  int pivot, l_hold, r_hold;
-  
-  l_hold = left;
-  r_hold = right;
-  pivot = numbers[left];
-  while (left < right) {
-    while ((numbers[right] >= pivot) && (left < right))
-      right--;
-    if (left != right) {
-      numbers[left] = numbers[right];
-      left++;
-    }
-    while ((numbers[left] <= pivot) && (left < right))
-      left++;
-    if (left != right) {
-      numbers[right] = numbers[left];
-      right--;
-    }
-  }
-  numbers[left] = pivot;
-  pivot = left;
-  left = l_hold;
-  right = r_hold;
-  if (left < pivot)
-    q_sort(numbers, left, pivot-1);
-  if (right > pivot)
-    q_sort(numbers, pivot+1, right);
-}
-
-
 void quicksort(double numbers[], int array_size)
 {
-  q_sort(numbers, 0, array_size - 1);
+  int i, j;
+  int min, temp;
+  
+  for (i = 0; i < array_size-1; i++) {
+    min = i;
+    for (j = i+1; j < array_size; j++) {
+      if ( numbers[ j ] < numbers[ min ] )
+        min = j;
+    }
+    temp = numbers[ i ];
+    numbers[ i ] = numbers[ min ];
+    numbers[ min ] = temp;
+  }
 }

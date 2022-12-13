07f6e24e27cf4ce4aebf5e44fb72cd608e82a4c5
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 16:03:13 -0800

    prelogue

--- assignments/final/display.asm
+++ assignments/final/display.asm
@@ -12,6 +12,23 @@ extern printf
 
 display:
 
+    push  rbp
+    mov   rsp, rbp
+    push  rdi
+    push  rsi
+    push  rdx
+    push  rcx
+    push  r8
+    push  r9
+    push  r10
+    push  r11
+    push  r12
+    push  r13
+    push  r14
+    push  r15
+    push  rbx
+    pushf
+
     xor rcx, rcx
     mov r8,  rdi ; array
     mov r9, rsi ; count
@@ -29,5 +46,20 @@ loop:
     jmp loop
 
 done:
+    popf
+    pop   rbx
+    pop   r15
+    pop   r14
+    pop   r13
+    pop   r12
+    pop   r11
+    pop   r10
+    pop   r9
+    pop   r8
+    pop   rcx
+    pop   rdx
+    pop   rsi
+    pop   rdi
+    pop   rbp
 
     ret

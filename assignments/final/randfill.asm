c58f527755c076f6344c0c55219d7f24e990fe53
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 16:09:07 -0800

    what

--- assignments/final/randfill.asm
+++ assignments/final/randfill.asm
@@ -12,23 +12,10 @@ global fill
 
 fill:
 
-    push  rbp
-    mov   rsp, rbp
-    push  rdi
-    push  rsi
-    push  rdx
-    push  rcx
-    push  r8
-    push  r9
-    push  r10
-    push  r11
-    push  r12
-    push  r13
-    push  r14
-    push  r15
-    push  rbx
-    pushf
 
+
+    push rcx
+    push rax
     xor rcx, rcx
 generate:
     ; loop setup
@@ -45,21 +32,6 @@ generate:
     jmp generate
 
 done:
-
-    popf
-    pop   rbx
-    pop   r15
-    pop   r14
-    pop   r13
-    pop   r12
-    pop   r11
-    pop   r10
-    pop   r9
-    pop   r8
-    pop   rcx
-    pop   rdx
-    pop   rsi
-    pop   rdi
-    pop   rbp
-
+    pop rax
+    pop rcx
     ret

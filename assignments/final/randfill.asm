169ee48aebd42c04e7b0177734eb4b125a285d05
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 16:04:34 -0800

    a

--- assignments/final/randfill.asm
+++ assignments/final/randfill.asm
@@ -12,8 +12,23 @@ global fill
 
 fill:
 
-    push rcx
-    push rax
+    push  rbp
+    mov   rsp, rbp
+    push  rdi
+    push  rsi
+    push  rdx
+    push  rcx
+    push  r8
+    push  r9
+    push  r10
+    push  r11
+    push  r12
+    push  r13
+    push  r14
+    push  r15
+    push  rbx
+    pushf
+
     xor rcx, rcx
 generate:
     ; loop setup
@@ -30,6 +45,21 @@ generate:
     jmp generate
 
 done:
-    pop rax
-    pop rcx
+
+    popf
+    pop   rbx
+    pop   r15
+    pop   r14
+    pop   r13
+    pop   r12
+    pop   r11
+    pop   r10
+    pop   r9
+    pop   r8
+    pop   rcx
+    pop   rdx
+    pop   rsi
+    pop   rdi
+    pop   rbp
+
     ret
--- assignments/final/sum.asm
+++ assignments/final/sum.asm
@@ -8,6 +8,22 @@ segment .text
 global sum
 
 sum:
+    push  rbp
+    mov   rsp, rbp
+    push  rdi
+    push  rsi
+    push  rdx
+    push  rcx
+    push  r8
+    push  r9
+    push  r10
+    push  r11
+    push  r12
+    push  r13
+    push  r14
+    push  r15
+    push  rbx
+    pushf
 
     xorpd xmm0, xmm0
     mov   rcx,  rsi
@@ -23,5 +39,20 @@ loop:
     jnz loop
 
 end:
+    popf
+    pop   rbx
+    pop   r15
+    pop   r14
+    pop   r13
+    pop   r12
+    pop   r11
+    pop   r10
+    pop   r9
+    pop   r8
+    pop   rcx
+    pop   rdx
+    pop   rsi
+    pop   rdi
+    pop   rbp
 
     ret

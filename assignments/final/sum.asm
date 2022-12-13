1f7e0f0123c22b9395716126d922127ff9a8f6a2
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-12 16:32:29 -0800

    com

--- assignments/final/sum.asm
+++ assignments/final/sum.asm
@@ -8,22 +8,6 @@ segment .text
 global sum
 
 sum:
-    push  rbp
-    mov   rsp, rbp
-    push  rdi
-    push  rsi
-    push  rdx
-    push  rcx
-    push  r8
-    push  r9
-    push  r10
-    push  r11
-    push  r12
-    push  r13
-    push  r14
-    push  r15
-    push  rbx
-    pushf
 
     xorpd xmm0, xmm0
     mov   rcx,  rsi
@@ -39,20 +23,4 @@ loop:
     jnz loop
 
 end:
-    popf
-    pop   rbx
-    pop   r15
-    pop   r14
-    pop   r13
-    pop   r12
-    pop   r11
-    pop   r10
-    pop   r9
-    pop   r8
-    pop   rcx
-    pop   rdx
-    pop   rsi
-    pop   rdi
-    pop   rbp
-
     ret
